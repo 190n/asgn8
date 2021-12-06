50d518227756a3e6c70741fc8e839cd3fa2c3608
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-05 21:05:57 -0800

    Fix BitVector

--- grass.c
+++ grass.c
@@ -1,9 +1,13 @@
 #include "field.h"
 
+#include <inttypes.h>
+#include <stdio.h>
+
 int main(void) {
-    Field *f = field_create(6);
-    field_touch_random(f, 4, 374);
+    Field *f = field_create(32);
+    field_touch_random(f, 48, 347928);
     field_print(f);
+    printf("%"PRIu32" writes\n", field_writes(f));
     field_delete(&f);
     return 0;
 }
